c998f1e2566d05d900dc092c7110680a7a25458d
veronikanos <vveronikanika@gmail.com>
2023-09-28 17:08:46 +0300

    fix: change svg as pseudoelement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,8 @@ import { ScrollToTopButton } from '@/components/ui/ScrollToTopButton';
 export default function Home() {
   return (
     <>
-      <div className="h-[700px] bg-stone-600"></div>
-      <div className="h-[800px] bg-stone-400"></div>
       <Countdown into="hero" />
       <ExampleSection />
-      <div className="h-[700px] bg-red-600"></div>
-      <div className="h-[800px] bg-red-400"></div>
       <ScrollToTopButton />
     </>
   );
--- src/components/ui/ScrollToTopButton/ScrollToTopButton.tsx
+++ src/components/ui/ScrollToTopButton/ScrollToTopButton.tsx
@@ -11,14 +11,13 @@ export const ScrollToTopButton = () => {
     <Link
       href="#top"
       className={`fixed bottom-10 right-10 bg-purple-light text-white rounded-full w-14 h-14 cursor-pointer hover:bg-purple-hover
-         shadow-button transition-opacity btn-transition ${
+         shadow-button transition-opacity btn-transition scroll-to-top-button ${
            isVisible ? 'opacity-100' : 'opacity-0'
          }`}
       onClick={scrollToTop}
       aria-label="Повернутись догори"
       role="button"
     >
-      ↑
     </Link>
   );
 };
