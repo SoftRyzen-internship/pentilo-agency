59db16a5bcb7ba2cad2dc5fd0566b8b9bc0cdd68
veronikanos <vveronikanika@gmail.com>
2023-09-28 14:43:33 +0300

    resolve conflicts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,22 @@
 import { ExampleSection } from '@/views/ExampleSection';
 
 import { Countdown } from '@/components/ui/Countdown';
-import {  ScrollToTopButton, ToTopButtonWrapper } from '@/components/ui/ScrollToTopButton';
+import {
+  ScrollToTopButton,
+  ToTopButtonWrapper,
+} from '@/components/ui/ScrollToTopButton';
 
 export default function Home() {
   return (
     <>
-    {/* <ToTopButtonWrapper> */}
-    <div className="h-[700px] bg-stone-600" ></div>
-    <div className="h-[800px] bg-stone-400" ></div>
-    
-      
-    <Countdown into="hero" />
-        <ExampleSection />
+      {/* <ToTopButtonWrapper> */}
+      <div className="h-[700px] bg-stone-600"></div>
+      <div className="h-[800px] bg-stone-400"></div>
+
+      <Countdown into="hero" />
+      <ExampleSection />
       {/* </ToTopButtonWrapper> */}
-      <ScrollToTopButton/>
+      <ScrollToTopButton />
     </>
   );
 }
