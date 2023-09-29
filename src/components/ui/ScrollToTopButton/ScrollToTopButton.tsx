f8feeadfb5b241b43cf405f25a18f9d08eb36e49
veronikanos <vveronikanika@gmail.com>
2023-09-29 15:10:50 +0300

    fix: change link to a

--- src/components/ui/ScrollToTopButton/ScrollToTopButton.tsx
+++ src/components/ui/ScrollToTopButton/ScrollToTopButton.tsx
@@ -2,21 +2,22 @@
 
 import { scrollToTop } from '@/utils/scrollToTop';
 import { useScrollVisibility } from '@/utils/useScrollVisibility';
-import Link from 'next/link';
+
+import css from './ScrollToTopButton.module.css';
 
 export const ScrollToTopButton = ({ sectionIdStart }) => {
   const isVisible = useScrollVisibility(sectionIdStart);
 
   return (
-    <Link
+    <a
       href="#top"
-      className={`fixed bottom-10 right-10 bg-purple-light text-white rounded-full w-14 h-14 cursor-pointer hover:bg-purple-hover
-         shadow-button transition-opacity btn-transition scroll-to-top-button ${
+      className={`${css.scrollBtn} fixed bottom-10 right-10 bg-purple-light text-white rounded-full w-14 h-14 cursor-pointer hover:bg-purple-hover
+         shadow-button transition-opacity btn-transition ${
            isVisible ? 'opacity-100' : 'opacity-0'
          }`}
       onClick={scrollToTop}
       aria-label="Повернутись догори"
       role="button"
-    ></Link>
+    ></a>
   );
 };
