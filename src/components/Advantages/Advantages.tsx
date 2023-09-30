2836f1741ac87046747033684e73802d5bb96606
Volodymyr-Bezyk <99117313+Volodymyr-Bezyk@users.noreply.github.com>
2023-09-30 08:14:27 +0300

    add advantages list

--- src/components/Advantages/Advantages.tsx
+++ src/components/Advantages/Advantages.tsx
@@ -1,16 +1,17 @@
-import Image from 'next/image';
-
 import React from 'react';
 
 import data from '@/data/advantages.json';
 import css from './Advantages.module.css';
 
-import { Icon } from '../ui/Icon';
+import { AdvantagesList } from '../AdvantagesList';
 
-export const Advantages = () => {
+export const Advantages: React.FC = () => {
   return (
     <div className=" w-full">
-      <h2 className=" mx-auto mb-4 max-w-[287px] text-center font-dela_gothic uppercase text-shadow-text md:mb-6 md:max-w-[486px] xl:mb-[15px] xl:w-full  xl:max-w-none xl:text-left">
+      <h2
+        className=" mx-auto mb-4 max-w-[287px] text-center font-dela_gothic uppercase 
+      text-shadow-text md:mb-6 md:max-w-[486px] xl:mb-[15px] xl:w-full  xl:max-w-none xl:text-left"
+      >
         <span className="block text-[34px] leading-[1.2] blur-[2.5px] xl:text-[52px] xl:blur-[3.43px]">
           {data.title.blur}
         </span>
@@ -21,26 +22,7 @@ export const Advantages = () => {
           {data.title.focus}
         </span>
       </h2>
-      <ul className="mx-auto grid max-w-[328px] gap-4 md:max-w-[380px] md:gap-6 xl:max-w-max xl:grid-cols-3 xl:gap-5">
-        {data.advantages.map(({ icon, text, path }, idx) => (
-          <li
-            key={idx}
-            className={`w-full max-w-[328px] rounded-[16px] px-[30px] pb-[30px] pt-[22px] md:h-[207px] md:max-w-[380px] md:pr-6 ${css.border}`}
-          >
-            <div className="mb-4 flex items-center justify-between">
-              <Image
-                src={path}
-                width={48}
-                height={48}
-                alt={icon}
-                className=" h-[48px] w-[48px]"
-              />
-              <Icon icon="arrowDown" className="h-[12px] w-[12px]" />
-            </div>
-            <p className=" text-s_big md:text-l_big">{text}</p>
-          </li>
-        ))}
-      </ul>
+      <AdvantagesList />
     </div>
   );
 };
