31b4a21c9d168bea3db03e9247ed8b4720fab7f0
veronikanos <vveronikanika@gmail.com>
2023-09-30 21:05:51 +0300

    fix: navigation and pagination implementing

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -13,6 +13,7 @@ import 'swiper/css/pagination';
 import 'swiper/css/effect-coverflow';
 import { SliderProps } from './types';
 import { useEffect, useRef } from 'react';
+import { SwiperNavButtons } from './SwiperNavButton';
 
 
 export const Slider: React.FC<SliderProps> = ({ section, data, element: Element, navigation}) => {
@@ -35,23 +36,53 @@ export const Slider: React.FC<SliderProps> = ({ section, data, element: Element,
     <Swiper
     ref={swiperRef}
     // updateOnWindowResize={true}
+
+
     effect={'coverflow'}
     grabCursor={true}
     centeredSlides={true}
-    spaceBetween={20}
+    // spaceBetween={20}
+    speed={500}
     // loop={true}
-    initialSlide={1}
-    slidesPerView={'auto'}
+    // initialSlide={1}
+    // slidesPerView={3}
+    // centeredSlidesBounds={true}
+
+    breakpoints={{
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 0,
+            // slideToClickedSlide: true,
+            effect: 'slide',
+            initialSlide: 0,
+            autoplay: true,
+            loop: true,
+        },
+        1440: {
+            slidesPerView: 3,
+            effect: 'coverflow',
+            spaceBetween: 20,
+            initialSlide: 1,
+            autoplay: false,
+            loop: false,
+        }
+    }}
+  
     coverflowEffect={{
       rotate: 0,
       stretch: 0,
       depth: 100,
       scale: 0.9,
       modifier: 1.6,
+      slideShadows: false,
     }}
     pagination={{
         el: '.swiper-pagination',
         clickable: true,
+        // renderBullet: function (index, className) {
+        //     return '<span class="' + className + '">' + index + '</span>';
+        //   },
       }}
       
 
@@ -73,23 +104,28 @@ export const Slider: React.FC<SliderProps> = ({ section, data, element: Element,
     //   }
 
 
-    modules={[EffectCoverflow, Navigation]}
+    modules={[EffectCoverflow, Navigation, Pagination, Autoplay]}
     className=" swiperStyles"
   >
+
+    <div className="wrapper bg-slate-400">
 {data?.map((item: any, idx: number) => { 
     return (
-      <SwiperSlide key={idx} className="gallerySlide z-10">
-          <div className="w-[380px]">
+      <SwiperSlide key={idx} className=" z-10">
+          {/* <div className=" slideWrapper"> */}
+          <Element {...item} className="elementSlider"/>
 
-          <Element {...item} />
-
-            </div>
+            {/* </div> */}
       </SwiperSlide>
     );
 })}
+</div>
+{/* <SwiperNavButtons /> */}
       <div className="slider-controller">
-        <div className="swiper-button-prev slider-arrow"></div>
-        <div className="swiper-button-next slider-arrow"></div>
+        {/* <div className="wrap"> */}
+            <div className="swiper-button-prev slider-arrow"></div>
+            <div className="swiper-button-next slider-arrow"></div>
+        {/* </div> */}
         <div className="swiper-pagination"></div>
        </div>
   </Swiper>
