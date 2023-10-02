2dd42fc28035f0e257a54ab452a28eb065d556f7
andrey-lawyer <andrey-1@ukr.net>
2023-10-02 13:12:23 +0300

    fix path

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,5 @@
 export * from './getFormattedTime';
 export * from './smoothScroll';
-export * from './scrollToTop';
 export * from './useScrollVisibility';
 export * from './notify';
 export * from './sendDataToTelegram';
