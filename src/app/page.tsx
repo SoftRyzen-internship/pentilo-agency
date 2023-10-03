ff25aad9e43248b819a7dc01a02567f45236f8d1
andrey-lawyer <andrey-1@ukr.net>
2023-10-03 20:01:47 +0300

    put section about in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@
 import { ExpertiseSection } from '@/views/ExpertiseSection';
 import { ApplyFormSection } from '@/views/ApplyFormSection';
 import { ScrollToTopButton } from '@/components/ui/ScrollToTopButton';
+import { AboutSection } from '@/views/AboutSection';
 
 export default function Home() {
   return (
     <>
       {/* <ExampleSection />
       <ScrollToTopButton sectionIdStart="advantages" /> */}
+      <AboutSection />
       <ExpertiseSection />
       <ApplyFormSection />
       <ScrollToTopButton sectionIdStart="advantages" />
