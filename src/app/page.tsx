10456142100b6abe0246fb397de71239c7b2139a
veronikanos <vveronikanika@gmail.com>
2023-10-11 13:13:54 +0300

    comment ServicesSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
       <AdvantagesSection />
       <AboutSection />
       <ExpertiseSection />
-      <ServicesSection />
+      {/* <ServicesSection /> */}
       <TeamSection />
       <CasesSection />
       <TrustSection />
